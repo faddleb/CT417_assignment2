02db75d43fe2e1764d0d1a0c494f7da4c7e0038f
faddleb <faddlebadiane@gmail.com>
2019-11-16 15:11:11 +0000

    Added unit tests for Module class

---
+++ src/test/java/nuigalway/app/ModuleTest.java
@@ -0,0 +1,36 @@
+package nuigalway.app;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import java.util.Date;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+public class ModuleTest 
+{
+    // Mock variables
+    String mockModuleName = "Software Engineering";
+    String mockId = "CT417";
+    Course mockCourse = mock(Course.class);
+    Student mockStudent = mock(Student.class);
+
+    // Mock arrays
+    Course[] mockCourseList = {mockCourse, mockCourse};
+    Student[] mockStudentList = {mockStudent, mockStudent};
+
+    @Test
+    public void shouldConstructModuleObjectWithParameters()
+    {   
+        Module module = new Module(mockModuleName, mockId, mockStudentList, mockCourseList);
+
+        assertTrue(module instanceof Module);
+        assertEquals(module.getModuleName(), mockModuleName);
+        assertEquals(module.getId(), mockId);
+        assertArrayEquals(module.getStudents(), mockStudentList);
+        assertArrayEquals(module.getCourses(), mockCourseList);
+    }
+}
